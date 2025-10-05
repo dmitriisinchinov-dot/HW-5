c4f033e13c274ec0c833ff198093be809b33a20c
Dmitrii Sinchinov <dmitriisinchinov@gmail.com>
2025-10-05 12:42:16 +0300

    HW 5 start

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Task 1
+        System.out.println("Task 1");
+        int[] winner = new int[3];
+        winner[0] = 1;
+        winner[1] = 2;
+        winner[2] = 3;
+        System.out.println(winner[0]);
+        System.out.println(winner[1]);
+        System.out.println(winner[2]);
+    }
+}
